e51ceab449566c2ae2fec8d90159e105e9fd9d6b
Nathan7594 <nathan.75@outlook.com>
2023-06-21 13:59:12 +0200

    Meme push que celui juste avant ce reféré au message commit juste avant

--- client/app.js
+++ client/app.js
@@ -1,4 +1,13 @@
 function myFunction() {
-    let inputValue = document.getElementById("myInput").value;
-    alert("Vous avez saisi : " + inputValue);
-  }
+  let inputValue = document.getElementById("myInput").value;
+  fetch("http://localhost:8000/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(inputValue),
+    })
+    .catch((error) => {
+      console.error("Erreur lors de la requête :", error);
+    });
+}
