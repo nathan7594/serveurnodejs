740e7546f61040a18ea3808631e30d964c790c39
Guillaume <guillaumepages@outlook.com>
2023-06-23 14:37:38 +0200

    refactor function app.post et app.get

--- serveur_express/serveur/serveur.js
+++ serveur_express/serveur/serveur.js
@@ -1,28 +1,26 @@
 import express from "express";
-import serveIndex from "serve-index";
 import { insert } from "./insert.js";
 import { select } from "./select.js";
 
 const app = express();
 app.use("/", express.static("../client/"));
-app.use("/public", serveIndex("../"));
 app.use(express.json());
 
 
-app.post("/post", (req, res) => {
+app.post("/api", (req, res) => {
   const { value } = req.body;
   insert(value);
+  // INSERT INTO name VALUE ('abc')
   res.status(200).send("Poste reçu avec succès");
 });
 
-app.get("/get", (req, res, next) => {
+app.get("/api", (req, res) => {
   console.log("coucou");
   select((error, results) => {
     console.log("c'est error",error)
     console.log("c'est result",results)
     res.end(JSON.stringify(results))
   })
-  // next()
 });
 
 app.listen(8000, () => console.log("Example app is listening on port 8000."));
