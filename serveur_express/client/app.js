3dbeeb3ea2198d4f5b47448b0fb21cf44fcd644d
Guillaume <guillaumepages@outlook.com>
2023-06-23 12:17:53 +0200

    Réception de la data depuis la BDD, envoie au client via une méthode get depuis le serveur

--- serveur_express/client/app.js
+++ serveur_express/client/app.js
@@ -1,49 +1,38 @@
 function insert() {
   const inputValue = document.getElementById("myInput").value;
   const postData = {
-    value : inputValue}
-  fetch("http://localhost:8000/", {
+    value: inputValue,
+  };
+  fetch("http://localhost:8000/post", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify(postData)
-    })
-    .catch((error) => {
-      console.error("Erreur lors de la requête :", error);
-    });
+    body: JSON.stringify(postData),
+  }).catch((error) => {
+    console.error("Erreur lors de la requête :", error);
+  });
 }
-// function insert(){
-//   const url = "http://localhost:8000/";
-//   let inputValue = document.getElementById("myInput").value;
-//   const postData = {inputValue}
-  
-//   let request = new Request(url, {
-//     method: 'POST',
-//     body: JSON.stringify(postData),
-//     headers: new Headers()
-//   });
-  
-//   fetch(request)
-// }
 
 const received = async () => {
   try {
-    const response = await fetch('http://localhost:8000/');
-    
+    const response = await fetch("http://localhost:8000/get");
+
     if (response.ok) {
       const data = await response.json();
-      console.log('Données reçues du serveur distant :', data);
-      const container = document.getElementById("texte")
+      console.log("Données reçues du serveur distant :", data);
+      const container = document.getElementById("texte");
 
       data.forEach((phrase, index) => {
         console.log(`Phrase ${index + 1}: ${phrase.texte}`);
-        container.innerHTML = phrase.texte
-      })
+        container.innerHTML = phrase.texte;
+      });
     } else {
-      console.log('Erreur lors de la récupération des données du serveur distant.');
+      console.log(
+        "Erreur lors de la récupération des données du serveur distant."
+      );
     }
   } catch (error) {
-    console.log('Une erreur s\'est produite :', error);
+    console.log("Une erreur s'est produite :", error);
   }
-}
+};
--- serveur_express/serveur/serveur.js
+++ serveur_express/serveur/serveur.js
@@ -1,17 +1,28 @@
 import express from "express";
 import serveIndex from "serve-index";
 import { insert } from "./insert.js";
+import { select } from "./select.js";
 
 const app = express();
-app.use('/', express.static('../client/'));
+app.use("/", express.static("../client/"));
 app.use("/public", serveIndex("../"));
 app.use(express.json());
-app.post('/', (req, res) => {
-  const {value} = req.body
-  insert(value)
-  // Récupère les données envoyées dans le corps de la requête
-  // Effectue les opérations souhaitées avec les données du poste
-  // Par exemple, vous pouvez les enregistrer dans une base de données ou effectuer d'autres traitements
-  res.status(200).send('Poste reçu avec succès');
+
+
+app.post("/post", (req, res) => {
+  const { value } = req.body;
+  insert(value);
+  res.status(200).send("Poste reçu avec succès");
 });
+
+app.get("/get", (req, res, next) => {
+  console.log("coucou");
+  select((error, results) => {
+    console.log("c'est error",error)
+    console.log("c'est result",results)
+    res.end(JSON.stringify(results))
+  })
+  // next()
+});
+
 app.listen(8000, () => console.log("Example app is listening on port 8000."));
